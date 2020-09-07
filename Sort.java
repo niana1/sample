8232d0ce12e2934ebc7386d24cd238809a0a0a8e
suchithra reddy <suchithranathi@gmail.com>
2020-09-07 09:43:45 +0530

    file added

---
+++ Sort.java
@@ -0,0 +1,16 @@
+package sapient;
+
+import java.util.Comparator;
+
+public class Sort implements Comparator {
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		// TODO Auto-generated method stub
+		String arr1[] = ((String) o1).split(",");
+		String arr2[] = ((String) o2).split(",");
+		return (int)Double.parseDouble(arr1[2]) - (int)Double.parseDouble(arr2[2]);
+		
+	}
+
+}
